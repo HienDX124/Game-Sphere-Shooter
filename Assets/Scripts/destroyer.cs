941360f8e0fd4507868fbb4c0f5257b16eb5b553
HienDX124 <xuanhien2000678354@gmail.com>
2022-01-11 00:56:38 +0700

    Page: 193

---
+++ Assets/Scripts/destroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyer : MonoBehaviour
+{
+    public float delay = 3.0f;
+    void Start()
+    {
+        Destroy(this.gameObject, delay);
+    }
+}
